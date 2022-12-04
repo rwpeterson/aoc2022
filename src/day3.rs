099d8bf8fd80e76fac27de805830117ff083c65f
Bob Peterson <bob@rwp.is>
2022-12-04 14:14:22 -0600

    Day 3 and revamped project organization

---
+++ src/day3.rs
@@ -0,0 +1,66 @@
+use anyhow::Result;
+use either::{Either, Left, Right};
+use itertools::Itertools;
+use std::collections::HashSet;
+
+pub fn main(input: &str) -> Result<Either<String, (String, String)>> {
+    let prio_sum: u32 = input
+        .lines()
+        .map(|l| {
+            let n = l.len();
+            let mut a = HashSet::new();
+            let mut b = HashSet::new();
+            for (i, c) in l.chars().enumerate() {
+                if i < n / 2 {
+                    a.insert(c);
+                } else {
+                    b.insert(c);
+                }
+            }
+            item_priority(*a.intersection(&b).next().expect("No overlap between rucksack compartments"))
+        })
+        .sum();
+
+    let s = prio_sum.to_string();
+
+    let badges = input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .filter_map(|group| {
+            let mut inventories = vec![HashSet::new(); 3];
+            for (i, rucksack) in group.enumerate() {
+                for item in rucksack.chars() {
+                    inventories[i].insert(item);
+                }
+            }
+            let mut badge_set = inventories[0].clone();
+            for inventory in inventories.iter().skip(1) {
+                badge_set = badge_set.intersection(inventory).cloned().collect();
+            }
+            let badge = badge_set.into_iter().next();
+            badge
+
+        })
+        .collect::<Vec<_>>();
+
+    let badge_sum: u32 = badges
+        .iter()
+        .map(|b| {
+            item_priority(*b)
+        })
+        .sum();
+
+    let t = badge_sum.to_string();
+
+    Ok(Right((s, t)))
+}
+
+fn item_priority(c: char) -> u32 {
+    let ascii = c as u32;
+    if c.is_uppercase() {
+        return ascii - 65 + 27
+    } else {
+        return ascii - 97 + 1
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,80 @@
 use anyhow::{Result, Context};
-use std::fs::File;
-use std::io::{self, BufRead};
+use either::{Either, Left, Right};
+use std::fs::read_to_string;
+
+mod day3;
+
+const HELP: &str = "\
+Bob - Advent of Code
+USAGE:
+  aoc [OPTIONS] DAY
+FLAGS:
+  -h, --help            Prints help information
+OPTIONS:
+  --part NUM            Specifies only one part
+ARGS:
+  DAY
+";
+
+#[derive(Debug)]
+struct AppArgs {
+    first: bool,
+    example: bool,
+    day: u32
+}
 
 fn main() -> Result<()> {
-    let f = File::open("p1.txt")?;
-    let lines = io::BufReader::new(f).lines();
-    let mut v: Vec<u64> = Vec::new();
-    for rline in lines {
-        let line = rline?;
-        if !(line.is_empty()) {
-            let cals = str::parse::<u64>(&line)?;
-            if let Some(x) = v.last_mut() {
-                *x += cals
-            } else {
-                v.push(cals);
-            }
-        } else {
-            v.push(0);
+    let args = match parse_args() {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Error: {}.", e);
+            std::process::exit(1);
         }
+    };
+
+    let filepath = format!(
+        "{}/day{}.txt",
+        if args.example { "example" } else { "input" },
+        args.day,
+    );
+
+    let input = read_to_string(&filepath)?;
+
+    let output = match args.day {
+        3 => day3::main(&input)?,
+        x => {
+            eprintln!("Error: Day {} not implemented", x);
+            std::process::exit(1);
+        }
+    };
+
+    match output {
+        Left(s) => println!("{}", s),
+        Right((s, t)) => println!("{}\n{}", s, t),
     }
-    println!("Most calories held: {}", v.iter().max().context("nobody is carrying any food")?);
-    let mut sum = 0;
-    let number = 3;
-    for _ in 0..number {
-        let maxcals = v.iter().max().context("nobody is carrying any food")?;
-        let pos = v.iter().position(|x| x == maxcals).context("max needs to be present")?;
-        sum += v.swap_remove(pos);
-    }
-    println!("Sum of top {} calories held: {}", number, sum);
     Ok(())
+}
+
+fn parse_args() -> Result<AppArgs, pico_args::Error> {
+    let mut pargs = pico_args::Arguments::from_env();
+
+    // Help has a higher priority and should be handled separately.
+    if pargs.contains(["-h", "--help"]) {
+        print!("{}", HELP);
+        std::process::exit(0);
+    }
+
+    let args = AppArgs {
+        first: pargs.contains(["-f", "--first"]),
+        example: pargs.contains(["-e", "--example"]),
+        day: pargs.free_from_str()?,
+    };
+
+    // It's up to the caller what to do with the remaining arguments.
+    let remaining = pargs.finish();
+    if !remaining.is_empty() {
+        eprintln!("Warning: unused arguments left: {:?}.", remaining);
+    }
+
+    Ok(args)
 }
